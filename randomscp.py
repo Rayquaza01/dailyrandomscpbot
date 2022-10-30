c91a979292a5527c816f150db2eae04d9ab721be
Joe Jarvis <joe.jarvis109@gmail.com>
2022-10-30 15:58:45 -0400

    Rework script to be more manual

--- randomscp.py
+++ randomscp.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python3
-import json
-import pytumblr
 import pyscp
+import pyperclip
 import random
 
 
 def randomSCP():
-    with open("scp.txt", "r") as scp:
-        choices = scp.read().split("\n")
-    choices.pop()  # remove last item, always empty string
-    choice = random.choice(choices)
-    choices.remove(choice)
-    with open("scp.txt", "w") as scp:
-        scp.write("\n".join(choices))
-    return choice
+    num = random.randint(1, 7999)
+    link = "scp-" + str(num).zfill(3)
+    return link
 
 
 def main():
-    wiki = pyscp.wikidot.Wiki("www.scp-wiki.net")
-    with open("tumblr_creds.json", "r") as js:
-        creds = json.loads(js.read())
-    client = pytumblr.TumblrRestClient(
-        creds["consumer_key"],
-        creds["consumer_secret"],
-        creds["oauth_token"],
-        creds["oauth_secret"]
-    )
-    rand = wiki(randomSCP())
-    client.create_link("dailyrandomscp", title=rand.title, url=rand.url, state="published",
-                       tags=["scp"], description="Today's random SCP of the day is: " + rand.title)
+    wiki = pyscp.wikidot.Wiki("scp-wiki.wikidot.com")
+
+    # hack to retry if generates a bad article
+    while True:
+        try:
+            rand = wiki(randomSCP())
+            title = rand.title
+            url = rand.url
+        except:
+            print(rand + " failed!")
+            continue
+        break
+
+    post = "# Today's random SCP of the day is [" + title + "](" + url + ")"
+
+    print(post)
+    pyperclip.copy(post)
 
 
 if __name__ == "__main__":
