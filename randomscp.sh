8a938172b4a9948d55dd4437953e93b033162480
Arnaught <rayquaza01@outlook.com>
2025-10-05 18:24:15 -0400

    Remove old version

--- randomscp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[@]}")")
-cd "$dir" || exit
-
-source "$dir/scpbot-3.10/bin/activate"
-
-if [ $# -eq 1 ]; then
-    python "$dir/randomscp.py" "$1"
-fi
